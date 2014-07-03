5f49648cae29a2388148fe6984246bc6eb595700
Dat Nguyen <nguyen.dat@bcgdv.com>
2014-07-03 10:33:03 -0700

    first commit

---
+++ SampleApp/ScanExample/Constants.h
@@ -0,0 +1,14 @@
+//
+//  Constants.h
+//  ScanExample
+//
+//  Copyright (c) 2013 PayPal. All rights reserved.
+//
+
+#ifndef ScanExample_Constants_h
+#define ScanExample_Constants_h
+
+// get your app token from the card.io website
+#define CardIOAppToken      @"6ce7e3b0cd3a41a3b2b7fc0cac62619e"
+
+#endif
